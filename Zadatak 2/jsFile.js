950e23667a1d309bd3d281c72dfa4805282a69e1
Suzana Milicev <suzana.milicev16@gmail.com>
2023-12-02 15:25:50 +0100

    Gotova oba zadatka

--- Zadatak 2/jsFile.js
+++ Zadatak 2/jsFile.js
@@ -7,14 +7,44 @@ $("#buttonShow").click(function () {
     $("#form-input").toggle("normal");
 });
 
+$("td[class=edit]").on("click", function () {
+    let newInfo = prompt("Izmena:");
+
+    if (newInfo) {
+        $(this).html(newInfo);
+    }
+
+    else {
+        alert("Kliknuli ste Cancel dugme");
+    }
+});
+
 
 $("#addButton").on("click", addRow);
 
 function addRow() {
-    var newRow = "<tr>" + "<td>" + $("#firstname").val() +
-        "</td>" + "<td>" + $("#lastname").val() + "</td>" + "<td>" + $("#email").val() +
-        "</td>" + "<td class='text-secondary fw-bold remove'>x</td>" + "</tr>";
+    var newRow =
+        "<tr>" +
+        "<td class='edit'>" + $("#firstname").val() + "</td>" +
+        "<td class='edit'>" + $("#lastname").val() + "</td>" +
+        "<td class='edit'>" + $("#email").val() + "</td>" +
+        "<td class='text-secondary fw-bold remove'>x</td>" +
+        "</tr>";
     $(newRow).appendTo("#mainTable");
+    $(document).on("click", ".remove", function () {
+        $(this).closest("tr").remove();
+    });
+    $(document).on("click", ".edit", function () {
+        let newInfo = prompt("Izmena:");
+
+        if (newInfo) {
+            $(this).html(newInfo);
+        }
+    
+        else {
+            alert("Kliknuli ste Cancel dugme");
+        }
+    });
     $("#firstname").val('');
     $("#lastname").val('');
     $("#email").val('');

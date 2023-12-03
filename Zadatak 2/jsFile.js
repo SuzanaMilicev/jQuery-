e576b46eb2dde0b8a9f079df6a25d3521bee2c02
Suzana Milicev <suzana.milicev16@gmail.com>
2023-12-03 08:37:36 +0100

    Konacna verzija

--- Zadatak 2/jsFile.js
+++ Zadatak 2/jsFile.js
@@ -25,22 +25,22 @@ $("#addButton").on("click", addRow);
 function addRow() {
     var newRow =
         "<tr>" +
-        "<td class='edit'>" + $("#firstname").val() + "</td>" +
-        "<td class='edit'>" + $("#lastname").val() + "</td>" +
-        "<td class='edit'>" + $("#email").val() + "</td>" +
+        "<td class='editNew'>" + $("#firstname").val() + "</td>" +
+        "<td class='editNew'>" + $("#lastname").val() + "</td>" +
+        "<td class='editNew'>" + $("#email").val() + "</td>" +
         "<td class='text-secondary fw-bold remove'>x</td>" +
         "</tr>";
     $(newRow).appendTo("#mainTable");
     $(document).on("click", ".remove", function () {
         $(this).closest("tr").remove();
     });
-    $(document).on("click", ".edit", function () {
-        let newInfo = prompt("Izmena:");
+    $(document).on("click", ".editNew", function () {
+        let newInfo2 = prompt("Izmena:");
 
-        if (newInfo) {
-            $(this).html(newInfo);
+        if (newInfo2) {
+            $(this).html(newInfo2);
         }
-    
+
         else {
             alert("Kliknuli ste Cancel dugme");
         }
